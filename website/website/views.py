cab28a4bb432570f32f225167f1f67ecaeb515ac
Serepok <gromyko-04@bk.ru>
2023-12-09 21:31:00 +1000

    upload

--- website/website/views.py
+++ website/website/views.py
@@ -6,11 +6,14 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'AboutUs/index.html')
 
+
 def news(request):
     return render(request, 'News/index.html')
 
+
 def docs(request):
     return render(request, 'Documents/index.html')
+
 
 def create_account(request):
     return render(request, 'registration/create_account.html')
